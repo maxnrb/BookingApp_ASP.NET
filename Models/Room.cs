e599f384cdff8d0b08d2ba07601e99567fbe611c
maxna <maxnarbaud@gmail.com>
2021-04-02 02:55:07 +0200

    Add room model

---
+++ Models/Room.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookingApp.Models
+{
+    public enum RoomTypes
+    {
+        Bedroom,
+        Bathroom,
+        Kitchen
+    }
+
+    public class Room
+    {
+        public Guid Id { get; set; }
+
+        public Guid AccommodationId { get; set; }
+        public virtual Accommodation Accommodation { get; set; }
+
+        public RoomTypes RoomType { get; set; }
+    }
+}
