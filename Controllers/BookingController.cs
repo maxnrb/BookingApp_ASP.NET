651b8287eb62b1e21236b1c75d419bb1cbfd7ad6
maxna <maxnarbaud@gmail.com>
2021-04-02 02:48:10 +0200

    Add booking functionality

---
+++ Controllers/BookingController.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using BookingApp.Data;
+using BookingApp.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+
+namespace BookingApp.Controllers
+{
+    [Authorize]
+    public class BookingController : Controller
+    {
+        private readonly AppContextDB _context;
+        private readonly UserManager<User> _userManager;
+
+        public BookingController(AppContextDB context, UserManager<User> userManager)
+        {
+            _context = context;
+            _userManager = userManager;
+        }
+
+        // GET: Booking
+        public async Task<IActionResult> Index()
+        {
+            var appContextDB = _context.Booking.Include(b => b.Offer).Include(b => b.User);
+            return View(await appContextDB.ToListAsync());
+        }
+
+        // GET: Booking/Details/5
+        public async Task<IActionResult> Details(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var booking = await _context.Booking
+                .Include(b => b.Offer)
+                .Include(b => b.User)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (booking == null)
+            {
+                return NotFound();
+            }
+
+            return View(booking);
+        }
+
+        // GET: Booking/Create
+        public IActionResult Create()
+        {
+            ViewData["OfferId"] = new SelectList(_context.Offers, "Id", "Id");
+            ViewData["UserId"] = new SelectList(_context.Users, "Id", "Id");
+            return View();
+        }
+
+        // POST: Booking/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(
+            [Bind("OfferId, ArrivalDate, ArrivalTime, DepartureDate, DepartureTime, NbPerson")] Booking booking)
+        {
+            if (ModelState.IsValid)
+            {
+                int nbNight = (booking.DepartureDate - booking.ArrivalDate).Days;
+                double pricePerNight = await _context.Offers.Where(o => o.Id == booking.OfferId).Select(o => o.PricePerNight).SingleOrDefaultAsync();
+                double cleaningFee = await _context.Offers.Where(o => o.Id == booking.OfferId).Select(o => o.CleaningFee).SingleOrDefaultAsync();
+
+                booking.TotalPrice = pricePerNight * (double)nbNight + cleaningFee;
+                booking.UserId = (await _userManager.GetUserAsync(User)).Id;
+
+                _context.Add(booking);
+                await _context.SaveChangesAsync();
+
+                return RedirectToAction(nameof(Index));
+            }
+
+            ViewData["OfferId"] = new SelectList(_context.Offers, "Id", "Id", booking.OfferId);
+            ViewData["UserId"] = new SelectList(_context.Users, "Id", "Id", booking.UserId);
+            return View(booking);
+        }
+
+
+        // GET: Booking/Delete/5
+        public async Task<IActionResult> Delete(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var booking = await _context.Booking
+                .Include(b => b.Offer)
+                .Include(b => b.User)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (booking == null)
+            {
+                return NotFound();
+            }
+
+            return View(booking);
+        }
+
+        // POST: Booking/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(Guid id)
+        {
+            var booking = await _context.Booking.FindAsync(id);
+            _context.Booking.Remove(booking);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool BookingExists(Guid id)
+        {
+            return _context.Booking.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ Models/Booking.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookingApp.Models
+{
+    public class Booking
+    {
+        public Guid Id { get; set; }
+
+        public Guid OfferId { get; set; }
+
+        [Display(Name = "Offre")]
+        public virtual Offer Offer { get; set; }
+
+        public string UserId { get; set; }
+
+        [Display(Name = "Utilisateur")]
+        public virtual User User { get; set; }
+
+        [Display(Name = "Date de réservation")]
+        [DataType(DataType.DateTime)]
+        public DateTime BookingDateTime { get; set; }
+
+        [Display(Name = "Date d'arrivé")]
+        [DataType(DataType.Date)]
+        public DateTime ArrivalDate { get; set; }
+
+        [Display(Name = "Horaire d'arrivé")]
+        [DataType(DataType.Time)]
+        public TimeSpan ArrivalTime { get; set; }
+
+        [Display(Name = "Date de départ")]
+        [DataType(DataType.Date)]
+        public DateTime DepartureDate { get; set; }
+
+        [Display(Name = "Horaire de départ")]
+        [DataType(DataType.Time)]
+        public TimeSpan DepartureTime { get; set; }
+
+        [Display(Name = "Nombre de voyageur(s)")]
+        public int NbPerson { get; set; }
+
+        [Display(Name = "Prix total")]
+        public double TotalPrice { get; set; }
+
+        public Booking()
+        {
+            this.BookingDateTime = DateTime.Now;
+        }
+	}
+}
